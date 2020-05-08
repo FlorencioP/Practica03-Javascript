ff765100d38b0c6983458e03dcdfb27af3d0d177
FlorencioP <sebasale10@hotmail.com>
2020-05-08 17:07:17 -0500

    Realizado el literal A del Formulario

--- ValidacionDatos/validaciones_usuario.js
+++ ValidacionDatos/validaciones_usuario.js
@@ -1,19 +1,125 @@
+
+var bandera = true 
+
+function validarCedula(){
+
+    //Validacion Cedula
+    var cad = document.getElementById('cedula').value.trim();
+        
+    console.log(cad)
+
+    var total = 0;
+    var longitud = cad.length;
+    
+    console.log(longitud)
+
+    var longcheck = longitud - 1;
+
+    if (cad !== "" && longitud === 10){
+        console.log("asdasd")
+        for(i = 0; i < longcheck; i++){
+            if (i%2 === 0) {
+            var aux = cad.charAt(i) * 2;
+            if (aux > 9) aux -= 9;
+            total += aux;
+            } else {
+            total += parseInt(cad.charAt(i));
+            }
+        }
+
+      total = total % 10 ? 10 - total % 10 : 0;
+      
+      console.log(total)
+      console.log(cad.charAt(longitud-1))
+
+
+        if (cad.charAt(longitud-1) == total) {
+            document.getElementById('mensajeCedula').innerHTML = '' 
+            cedula.style.border = '1px black solid' 
+            cedula.className = 'none' 
+            bandera = true
+        
+        }
+        else{
+    
+            document.getElementById('mensajeCedula').innerHTML = '<br>La Cedula Ingresada no es Valida' 
+            cedula.style.border = '1px red solid' 
+            cedula.className = 'error' 
+            bandera = false 
+
+        }
+    }
+    else{
+        document.getElementById('mensajeCedula').innerHTML = '<br>La Cedula esta mal ingresada' 
+        
+        cedula.style.border = '1px red solid' 
+        cedula.className = 'error' 
+        bandera = false 
+    }
+
+
+
+    
+
+}
+
+
+
+
+
+
 function validarCamposObligatorios() {
-    var bandera = true 
+    bandera = true 
      
     for(var i = 0; i < document.forms[0].elements.length; i++){
         var elemento = document.forms[0].elements[i] 
+
+
+
+        // Verificador de campos no vacios 
         if(elemento.value == '' && elemento.type == 'text'){
-           if(elemento.id == 'cedula'){
-                document.getElementById('mensajeCedula').innerHTML = '<br>La cedula esta vacia' 
-            } 
+
+
+            if(elemento.id == 'cedula'){
+                document.getElementById('mensajeCedula').innerHTML = '<br>Este Campo se encuentra Vacio'
+            }
+
+            if(elemento.id == 'nombres'){
+                document.getElementById('mensajeNombres').innerHTML = '<br>Este Campo se encuentra Vacio' 
+            }
+
+            if(elemento.id == 'apellidos'){
+                document.getElementById('mensajeApellido').innerHTML = '<br>Este Campo se encuentra Vacio' 
+            }
+
+            if(elemento.id == 'direccion'){
+                document.getElementById('mensajeDireccion').innerHTML = '<br>Este Campo se encuentra Vacio' 
+            }
+
+            if(elemento.id == 'telefono'){
+                document.getElementById('mensajeTelefono').innerHTML = '<br>Este Campo se encuentra Vacio' 
+            }
+
+            if(elemento.id == 'fechaNacimiento'){
+                document.getElementById('mensajeFecha').innerHTML = '<br>Este Campo se encuentra Vacio' 
+            }
+
+            if(elemento.id == 'correo'){
+                document.getElementById('mensajeCorreo').innerHTML = '<br>Este Campo se encuentra Vacio' 
+            }
+
+
             
-            elemento.style.border = '1px red solid' 
-            elemento.className = 'error' 
-            bandera = false 
         } 
+
+        if(elemento.value == '' && elemento.type == 'password'){
+            if(elemento.id == 'contrasena'){
+                document.getElementById('mensajeContra').innerHTML = '<br>Ingrese Una contraseña' 
+            }
+        }
     }
 
+    
     if(!bandera){ 
         alert('Error: revisar los comentarios') 
     } 
@@ -58,32 +164,3 @@ function validarNumeros(elemento) {
         return true 
     } 
 }
-
-
-
-function validar() {
-    var cad = document.getElementById("cedula").value.trim();
-    var total = 0;
-    var longitud = cad.length;
-    var longcheck = longitud - 1;
-
-    if (cad !== "" && longitud === 10){
-      for(i = 0; i < longcheck; i++){
-        if (i%2 === 0) {
-          var aux = cad.charAt(i) * 2;
-          if (aux > 9) aux -= 9;
-          total += aux;
-        } else {
-          total += parseInt(cad.charAt(i));
-        }
-      }
-
-      total = total % 10 ? 10 - total % 10 : 0;
-
-      if (cad.charAt(longitud-1) == total) {
-        document.getElementById("salida").innerHTML = ("Cedula Válida");
-      }else{
-        document.getElementById("salida").innerHTML = ("Cedula Inválida");
-      }
-    }
-  }
